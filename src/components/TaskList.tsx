1cd7e38ba82bde8a835a61d3ea015cb12252ec12
Francine Santos <65177834+francinesanto@users.noreply.github.com>
2022-09-06 11:20:37 -0300

    :tada: Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-  }
+    if(!newTaskTitle) return;
+    
+    const newTask  = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldTasks => [...oldTasks,newTask])
+    setNewTaskTitle('')
+} 
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const filteredTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete //invertendendo o valor que estava
+    }: task);
+
+    setTasks(filteredTask);//setando a task com o isComplete com o novo valor
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTask);// setando só as tasks com id diferente
   }
 
   return (
